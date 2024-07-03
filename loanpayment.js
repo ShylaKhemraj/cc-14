ebd9980676a794de26033cde21f99e84a2372ecc
Shyla Khemraj <shylakhemraj@usf.edu>
2024-07-02 23:43:44 -0400

    finalize.

--- loanpayment.js
+++ loanpayment.js
@@ -1,4 +1,4 @@
 export function calculateLoanpayment(principal, rate, n) {
     let rate = rate / 12;
-    return principal * (rate * Math.pow((1+rate), n)) / (math.pow((1+rate), n)-1);
+    return principal * (rate * Math.pow((1+rate), n)) / (math.pow((1+rate), n)-1)
 }
